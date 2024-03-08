251d9703cf38badb004e5b17a6eb0d969cfe7a32
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-03-08 15:21:32 -0300

    Criando seção "Nossos Clientes"

--- src/components/OurCustomers.jsx
+++ src/components/OurCustomers.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { CUSTOMERS_DATA } from '../data/customers';
 
 export const OurCustomers = () => {
+
+    const customersLogo = CUSTOMERS_DATA;
 
     return (
 
         <>
-            <div>
-                <h1>Our Customers</h1>
+            <div className='flex flex-col items-center justify-center font-openSans'>
+
+                <div className='flex flex-col justify-center items-center w-full max-w-[1000px]'>
+                    <div className='pt-24'>
+                        <h1 className='text-3xl font-bold text-red-600'>NOSSOS CLIENTES</h1>
+                    </div>
+
+                    <div className='grid grid-cols-3 gap-4 py-20'>
+
+                        {customersLogo.map((customer, index) => {
+                            return(
+                                <div key={index}>
+                                    <img src={customer.logo} alt="" />
+                                </div>
+                            )
+                        })}
+
+                    </div>
+                </div>
+
             </div>
         </>
 
---
+++ src/data/customers.js
@@ -0,0 +1,26 @@
+export const CUSTOMERS_DATA = [
+    {
+        id: 1,
+        logo: 'src/assets/img/logoipsum.png'
+    },
+    {
+        id: 2,
+        logo: 'src/assets/img/logoipsum.png'
+    },
+    {
+        id: 3,
+        logo: 'src/assets/img/logoipsum.png'
+    },
+    {
+        id: 4,
+        logo: 'src/assets/img/logoipsum.png'
+    },
+    {
+        id: 5,
+        logo: 'src/assets/img/logoipsum.png'
+    },
+    {
+        id: 6,
+        logo: 'src/assets/img/logoipsum.png'
+    },
+]
