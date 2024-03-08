c53dd4865dbd8da05b0d8879569c99cfbb801d3c
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-03-08 14:09:04 -0300

    Criando cardContent na pasta data

---
+++ src/data/cardsContent.js
@@ -0,0 +1,20 @@
+export const CARDS_CONTENT = [
+    {
+        id: 1,
+        icon: 'src/assets/icons/efficiency.svg',
+        title: 'EFICIÊNCIA',
+        text: 'Nossa abordagem focada na eficiência garante entregas pontuais e otimiza cada etapa do processo logístico. Economize tempo e recursos com a nossa eficiência operacional.'
+    },
+    {
+        id: 2,
+        icon: 'src/assets/icons/trust.svg',
+        title: 'CONFIANÇA',
+        text: 'Construímos confiança por meio de desempenho consistente. Com uma sólida reputação, garantimos a segurança e integridade de suas remessas. Sua carga, nossa responsabilidade.'
+    },
+    {
+        id: 3,
+        icon: 'src/assets/icons/inovation.svg',
+        title: 'INOVAÇÃO',
+        text: 'Impulsionamos o futuro da logística com soluções inovadoras. Da rastreabilidade avançada à tecnologia de ponta, estamos na vanguarda da inovação para oferecer resultados excepcionais.'
+    }
+]
