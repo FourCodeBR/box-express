024e9575b8fe648dd58f3a63dbeb0cfecd123468
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-03-09 00:08:21 -0300

    testando deploy

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -1,7 +1,7 @@
 export const SERVICES_DATA = [
     {
         id: 1,
-        img: 'src/assets/img/cargaFracionada.jpg',
+        img: '../assets/img/cargaFracionada.jpg',
         title: 'Carga Fracionada',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ipsum quam, tempor vestibulum velit quis, posuere egestas elit. Donec tincidunt, ligula id blandit consectetur, risus ligula viverra arcu, a rutrum leo ligula eu urna. Phasellus non tortor at nisl laoreet ultrices ac eget felis. Morbi tempus facilisis nulla interdum cursus.'
     },
