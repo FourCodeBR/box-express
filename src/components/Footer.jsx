9df3222de783abb74b451b19342912fdfcab8a44
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-03-12 20:12:04 -0300

    finalizando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,43 +9,38 @@ export const Footer = () => {
     return (
 
         <>
-            <div className='bg-red-600 flex flex-col justify-center py-20 font-openSans text-white'>
+            <div className='bg-red-600 flex flex-col justify-center md:py-12 py-10 font-openSans text-white'>
 
-                <div className='flex justify-center items-center'>
-                    <div className='grid grid-cols-3 w-full max-w-[1000px]'>
+                <div className='flex justify-center flex-start'>
+                    <div className='grid md:grid-cols-3 md:gap-0 gap-10 items-start w-full max-w-[1000px]'>
 
                         <div className='flex flex-col justify-center'>
-                            <h1 className='text-3xl text-center'>LINKS ÚTEIS</h1>
+                            <h1 className='text-3xl text-center font-bold'>LINKS ÚTEIS</h1>
+                            <div className='flex flex-col pt-5'>
+                                <a href="#" className='text-center hover:text-slate-300 duration-200'>INICÍO</a>
+                                <a href="#" className='text-center hover:text-slate-300 duration-200'>SOBRE</a>
+                                <a href="#" className='text-center hover:text-slate-300 duration-200'>CLIENTES</a>
+                                <a href="#" className='text-center hover:text-slate-300 duration-200'>TRABALHE CONOSCO</a>
+                                <a href="#" className='text-center hover:text-slate-300 duration-200'>CONTATO</a>
+                            </div>
                         </div>
 
                         <div className='flex flex-col justify-center'>
-                            <h1 className='text-3xl text-center'>SOBRE</h1>
+                            <h1 className='text-3xl text-center font-bold'>SOBRE</h1>
+                            <div className='pt-5'>
+                                <p className='text-center'>contato@fourcode.com.br</p>
+                                <p className='text-center'>Rua Diogo de Faria - 249</p>
+                                <p className='text-center'>Americana/Sp</p>
+                                <p className='text-center'>(19) 99999-9999</p>
+                            </div>
                         </div>
 
-                        <div className='flex flex-col justify-center'>
+                        <div className='flex flex-col items-center justify-center font-bold'>
                             <h1 className='text-3xl text-center'>SOCIAL</h1>
-                        </div>
+                            <div className='flex gap-2 flex-col pt-5'>
 
-
-                        <div className='flex flex-col  pt-5'>
-                            <a href="#" className='text-center hover:text-slate-300 duration-200'>INICÍO</a>
-                            <a href="#" className='text-center hover:text-slate-300 duration-200'>SOBRE</a>
-                            <a href="#" className='text-center hover:text-slate-300 duration-200'>CLIENTES</a>
-                            <a href="#" className='text-center hover:text-slate-300 duration-200'>TRABALHE CONOSCO</a>
-                            <a href="#" className='text-center hover:text-slate-300 duration-200'>CONTATO</a>
-                        </div>
-
-                        <div className='flex flex-col justify-center'>
-                            <p className='text-center'>contato@fourcode.com.br</p>
-                            <p className='text-center'>Rua Diogo de Faria - 249</p>
-                            <p className='text-center'>Americana/Sp</p>
-                            <p className='text-center'>(19) 99999-9999</p>
-                        </div>
-
-                        <div className='flex justify-center items-center'>
-                            <div className='grid grid-cols-2 gap-2'>
                                 <div className='w-8'>
-                                    <a href=""><img src={facebook} alt="" /></a>
+                                    <a href="" ><img src={facebook} alt="" /></a>
                                 </div>
                                 <div className='w-8'>
                                     <a href=""><img src={instagram} alt="" /></a>
@@ -60,6 +55,21 @@ export const Footer = () => {
                         </div>
 
                     </div>
+                </div>
+
+            </div>
+
+            <div className='bg-red-900 flex justify-center font-openSans text-white py-1 md:text-sm text-xs'>
+
+                <div className='flex justify-between w-full px-5 md:py-1 py-1'>
+
+                    <div>
+                        <h3 className='text-left'>Copyright © 2024 Box Express</h3>
+                    </div>
+                    <div>
+                        <h3 className='text-right'>Desenvolvido por : <a href="">Fourcode Technology</a></h3>
+                    </div>
+
                 </div>
 
             </div>
