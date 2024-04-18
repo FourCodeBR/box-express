47d7934d7cbf3d2e3e87241046f57a8fd830b7f8
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-04-18 12:00:23 -0300

    Ajuste nos cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -17,12 +17,16 @@ export const Cards = () => {
             >
 
                 {cardContent.map((content, index) => (
-                    <div key={index} className='flex flex-col text-center justify-around items-center h-[330px] w-[330px] rounded-md bg-red-600 md:mx-3 md:py-0 my-3 md:my-0 px-3 drop-shadow-lg'>
+                    <div key={index} className='flex flex-col text-center justify-around items-center h-[330px] w-[330px] rounded-md bg-red-600 md:mx-3 md:py-6 py-10 my-3 md:my-0 px-4 drop-shadow-lg'>
                         <div className='flex flex-col items-center gap-2'>
                             <img src={content.icon} alt={content.title} className='md:w-16 md:h-16 h-12 w-12' />
                             <h1 className='font-bold md:text-2xl text-xl'>{content.title}</h1>
                         </div>
-                        <span>{content.text}</span>
+
+                        <div className='h-full pt-10'>
+                            <span>{content.text}</span>
+                        </div>
+
                     </div>
                 ))}
 
